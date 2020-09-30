3cee33d02df55f78a82ff0c2798e9f3dbc92012d
adllcz <aobetkal@gmail.com>
2020-09-30 21:51:17 +0200

    form validation adjusted

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,18 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+import { validateEmail, validatePassword } from '../../helpers/formValidators/Validators/formValidator';
 
 import logo from '../../assets/images/logo.png';
 
 import './LoginForm.css';
 
-const LoginContainer = () => {
+const LoginForm = (): JSX.Element => {
+    const [hasEmailError, setHasEmailError] = useState<boolean>(false);
+    const [hasPasswordError, setHasPasswordError] = useState<boolean>(false);
+
+    useEffect(() => {
+
+    }, [hasEmailError, hasPasswordError]);
+
+    const handleFormSubmit = (event: FormEvent<HTMLFormElement>): void => {
+        event.preventDefault();
+
+        const {currentTarget: { email, password }} = event;
+
+        const emailValue: string = email.value;
+        const passwordValue: string = password.value;
+
+        setHasEmailError(!validateEmail(emailValue));
+        setHasPasswordError(!validatePassword(passwordValue));
+
+        if (hasEmailError || hasPasswordError) {
+            return; 
+        }
+
+    };
+
     return (
-        <Card>
+        <Card style={{maxWidth: '500px'}}>
             <div className="card-header-container">
                 <img 
                     className='card-header-logo' 
@@ -25,7 +52,7 @@ const LoginContainer = () => {
                 />
             </div>
             <CardContent>
-                <form>
+                <form onSubmit={(event: FormEvent<HTMLFormElement>) => handleFormSubmit(event)}>
                     <TextField
                         autoFocus
                         variant='outlined'
@@ -34,7 +61,14 @@ const LoginContainer = () => {
                         id='email'
                         label='Email'
                         name='email'
+                        autoComplete='email'
                     />
+                    {
+                        hasEmailError && 
+                        <Typography color='error' display='inline'>
+                            Provide a valid email address
+                        </Typography>
+                    }
                     <TextField
                         variant='outlined'
                         margin='normal'
@@ -43,7 +77,14 @@ const LoginContainer = () => {
                         label='Password'
                         type='password'
                         id='password'
+                        autoComplete='current-password'
                     />
+                    {
+                        hasPasswordError && 
+                        <Typography color='error' display='inline'>
+                            Password should have at least 8 characters (at least 1 numeric and 1 upper letter)
+                        </Typography>
+                    }
                     <Button
                         style={{marginTop: '30px'}}
                         className='card-header-button'
@@ -59,4 +100,4 @@ const LoginContainer = () => {
     );
 };
 
-export default LoginContainer;
+export default LoginForm;
