195adeadb09fa21bcf4080fa6c4256c2b01e1900
adllcz <aobetkal@gmail.com>
2020-10-01 20:06:30 +0200

    login form adjusted to use message component

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,15 +1,17 @@
-import React, { useState, useEffect, FormEvent } from 'react';
+import React, { useState, useEffect, FormEvent, ChangeEvent } from 'react';
 
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 import LinearProgress from '@material-ui/core/LinearProgress';
+
+import Message from '../Message/Message';
 
 import { validateEmail, validatePassword } from '../../helpers/formValidators/Validators/formValidator';
 import { requestHandler } from '../../helpers/requestHandlers/Handlers/requestHandler';
+
 import { RequestHandlerProps } from '../../helpers/requestHandlers/Handlers/requestHandlerProps';
 import { UserProps } from './LoginFormProps';
 
@@ -17,7 +19,10 @@ import logo from '../../assets/images/logo.png';
 
 import './LoginForm.css';
 
+
 const LoginForm = (): JSX.Element => {
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const [hasError, setHasError] = useState<boolean>(false);
     const [hasEmailError, setHasEmailError] = useState<boolean>(false);
@@ -25,26 +30,24 @@ const LoginForm = (): JSX.Element => {
     const [user, setUser] = useState<UserProps | null>(null);
 
     useEffect(() => {
-
+        if (user) {
+            handleFormClear();
+        }
     }, [user]);
 
     const handleFormSubmit = (event: FormEvent<HTMLFormElement>): void => {
         event.preventDefault();
         setHasError(false);
 
-        const {currentTarget: { email, password }} = event;
-
-        const emailValue: string = email.value;
-        const passwordValue: string = password.value;
-        const hasFormErrors: boolean = handleFormErrors(emailValue, passwordValue);
+        const hasFormErrors: boolean = handleFormErrors(email, password);
 
         if (hasFormErrors) {
             return; 
         }
 
         const requestHandlerProps: RequestHandlerProps = {
-            email: emailValue,
-            password: passwordValue,
+            email,
+            password,
             setIsLoading,
             setHasError,
             setUser
@@ -62,6 +65,11 @@ const LoginForm = (): JSX.Element => {
         setHasPasswordError(!isPasswordValidated);
 
         return hasErrors;
+    };
+    
+    const handleFormClear = () => {
+        setEmail('');
+        setPassword('');
     };
 
     return (
@@ -81,6 +89,9 @@ const LoginForm = (): JSX.Element => {
             <CardContent>
                 <form onSubmit={(event: FormEvent<HTMLFormElement>) => handleFormSubmit(event)}>
                     <TextField
+                        disabled={isLoading}
+                        onChange={(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => setEmail(event.currentTarget.value)}
+                        value={email}
                         autoFocus
                         variant='outlined'
                         margin='normal'
@@ -89,15 +100,16 @@ const LoginForm = (): JSX.Element => {
                         label='Email'
                         name='email'
                         autoComplete='email'
-                        disabled={isLoading}
                     />
-                    {
-                        hasEmailError && 
-                        <Typography color='error' display='inline'>
-                            Provide a valid email address
-                        </Typography>
-                    }
+                    <Message 
+                        type='error' 
+                        message='Provide a valid email address' 
+                        isDisplayed={hasEmailError} 
+                    />
                     <TextField
+                        disabled={isLoading}
+                        onChange={(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => setPassword(event.currentTarget.value)}
+                        value={password}
                         variant='outlined'
                         margin='normal'
                         fullWidth
@@ -106,14 +118,12 @@ const LoginForm = (): JSX.Element => {
                         type='password'
                         id='password'
                         autoComplete='current-password'
-                        disabled={isLoading}
                     />
-                    {
-                        hasPasswordError && 
-                        <Typography color='error' display='inline'>
-                            Password should have at least 8 characters (at least 1 numeric and 1 upper letter)
-                        </Typography>
-                    }
+                    <Message 
+                        type='error' 
+                        message='Password should have at least 8 characters (at least 1 numeric and 1 upper letter)' 
+                        isDisplayed={hasPasswordError} 
+                    />
                     <Button
                         style={{marginTop: '30px'}}
                         className='card-header-button'
@@ -125,12 +135,16 @@ const LoginForm = (): JSX.Element => {
                         Sign in
                     </Button>
                     { isLoading && <LinearProgress /> }
-                    {
-                        hasError && 
-                        <Typography color='error' display='inline'>
-                            User not found. Check your credentials and try again.
-                        </Typography>
-                    }
+                    <Message 
+                        type='primary' 
+                        message={user ? `Welcome ${user.name} ${user.surname}! Logged in.` : ''}
+                        isDisplayed={!!user} 
+                    />
+                    <Message 
+                        type='error' 
+                        message='User not found. Check your credentials and try again.' 
+                        isDisplayed={hasError} 
+                    />
                 </form>
             </CardContent>
         </Card>
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Container from '@material-ui/core/Container';
-import Grid, { } from  '@material-ui/core/Grid';
+import Grid from  '@material-ui/core/Grid';
 
 import LoginForm from '../../components/LoginForm/LoginForm';
 
